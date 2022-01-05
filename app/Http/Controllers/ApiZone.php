27836a419c7156eae6c5513254cb06f9fb4f7dde
Benjamin Krammel <bk@internetx.de>
2022-01-05 12:06:39 +0100

    add zone create example

--- app/Http/Controllers/ApiZone.php
+++ app/Http/Controllers/ApiZone.php
@@ -8,6 +8,12 @@ use Domainrobot\Lib\DomainrobotException;
 use Domainrobot\Model\Query;
 use Domainrobot\Model\QueryFilter;
 use Domainrobot\Model\QueryView;
+use Domainrobot\Model\Zone;
+use Domainrobot\Model\Soa;
+use Domainrobot\Model\MainIp;
+use Domainrobot\Model\NameServer;
+use Domainrobot\Model\NameserverActionConstants;
+use Domainrobot\Model\ResourceRecord;
 
 class ApiZone extends Controller
 {
@@ -35,6 +41,72 @@ class ApiZone extends Controller
             $domainrobot::getLastDomainrobotResult()->getResult(),
             $domainrobot::getLastDomainrobotResult()->getStatusCode()
         );
+    }
+
+    public function create(Request $request){
+        $domainrobot = app('Domainrobot');
+
+        $zone = new Zone();
+        $zone->setOrigin('example.com');
+        $zone->setSoa(new Soa([
+            "refresh" => 43200,
+            "retry" => 7200,
+            "expire" => 1209600,
+            "email" => "someone@example.com"
+        ]));
+
+        $zone->setAction(NameserverActionConstants::COMPLETE);
+        $zone->setNameServers([
+            new NameServer([
+                "name" => "a.ns14.net"
+            ]),
+            new NameServer([
+                "name" => "b.ns14.net"
+            ]),
+            new NameServer([
+                "name" => "c.ns14.net"
+            ]),
+            new NameServer([
+                "name" => "d.ns14.net"
+            ])
+        ]);
+
+        $zone->setResourceRecords([
+            new ResourceRecord([
+                "name" => "subdomain",
+                "type" => "A",
+                "value" => "198.51.100.1",
+                //"pref" => 1 // optional
+            ]),
+             new ResourceRecord([
+                "name" => "mail",
+                "type" => "A",
+                "value" => "198.51.100.1",
+                //"pref" => 1 // optional
+            ]),
+             new ResourceRecord([
+                "name" => "",
+                "type" => "MX",
+                "value" => "198.51.100.1",
+                "pref" => 10
+            ])
+        ]);
+
+        try {
+            // Domainrobot\Model\Contact
+            $contact = $domainrobot->zone->create($zone);
+        } catch (DomainrobotException $exception) {
+            return response()->json(
+                $exception->getError(),
+                $exception->getStatusCode()
+            );
+        }
+        
+        return response()->json(
+            $domainrobot::getLastDomainrobotResult()->getResult(),
+            $domainrobot::getLastDomainrobotResult()->getStatusCode()
+        );
+
     }
 
     /**
