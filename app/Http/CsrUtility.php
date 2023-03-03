0e1691cf8044ed420dd2d5bdd10a0a782887dd20
Christian Pleintinger <christian.pleintinger@internetx.com>
2023-03-03 06:38:24 +0100

    DEV-7742
    Signed-off-by: Christian Pleintinger <christian.pleintinger@internetx.com>

--- app/Http/CsrUtility.php
+++ app/Http/CsrUtility.php
@@ -10,18 +10,18 @@ class CsrUtility
      * @param  string $commonName
      * @return string $csrOut
      */
-    public function generateCsr($commonName) {
+    public function generateCsr($commonName, $privateKeyType = 'OPENSSL_KEYTYPE_EC', $curveName = 'prime256v1', $digestAlg = 'sha384') {
 
         $subject = [
             'commonName' => $commonName
         ];
 
         $privateKey = openssl_pkey_new([
-            'private_key_type' => OPENSSL_KEYTYPE_EC,
-            'curve_name' => 'prime256v1'
+            'private_key_type' => $privateKeyType,
+            'curve_name' => $curveName
         ]);
 
-        $csr = openssl_csr_new($subject, $privateKey, [ 'digest_alg' => 'sha384' ]);
+        $csr = openssl_csr_new($subject, $privateKey, [ 'digest_alg' => $digestAlg ]);
 
         openssl_csr_export($csr, $csrOut);
 
